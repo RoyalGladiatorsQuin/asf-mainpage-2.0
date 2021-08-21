172b124ece4cd2662838234cb2a2c49ed080c59e
沃克贰叁 <13916981947@163.com>
2021-08-22 00:39:59 +0800

    <refactor>:完善成分姬样式与功能

--- src/components/commonTools/commonTools.vue
+++ src/components/commonTools/commonTools.vue
@@ -7,7 +7,8 @@
       </div>
       <a-input @blur="zdQuery" v-model="zdInputMessage" placeholder="想到什么词就搜搜看"></a-input>
     </a-card>
-    <a-card id="cfQuerier" size="default" :head-style="{'text-align':'right'}">
+    <a-card id="cfQuerier" size="small" :head-style="{'text-align':'right'}"
+            :body-style="{'padding':'20px','padding-bottom':'0'}">
       <div slot="title">
         <a-icon type="book"/>
         成分姬
@@ -15,19 +16,30 @@
       <a-input-search @search="cfQuery" :loading="cfLoading" v-model="cfInputMessage"
                       placeholder="想到什么词就搜搜看"></a-input-search>
       <a-card :bordered="false" id="cfInnerCard" size="small"
-              :head-style="{'text-align':'left','border':'none','padding':'0'}">
+              :head-style="{'text-align':'left','border':'none','padding':'0'}" :body-style="{'padding-top':'0px'}">
         <div slot="title">
           <a-divider id="cfInnerCardHeaderDivider" type="vertical"></a-divider>
           关注VUP
         </div>
-        happy
-        <a-card id="cfQueryCellsWrapper" :bordered="false">
-          <!--          <a-card v-for="" :bordered="false"></a-card>-->
-          <a-card :bordered="false">
-            <a-avatar v-if="cfLoadedOnce" referrerPolicy="no-referrer" :src="this.$data.cfQueryList[0].face"></a-avatar>
-          </a-card>
+        <a-card :bordered="false" v-if="cfLoadedOnce" id="cfQueryCellsWrapper"
+                :body-style="{'padding':'0'}">
+          <!--          :body-style=" {
+                  'padding':'0','overflow-x': 'hidden','overflow-y':'auto','max-height':'100px'}"-->
+          <vue-scroll :ops="ops">
+            <a-comment id="cfQueryCell" v-for="data in cfQueryMessageJSONObject.list" v-bind:key="data.uname" :author="data.uname">
+              <a-avatar slot="avatar" id="cfQueryCellAvatar" :src="data.face"></a-avatar>
+              <p slot="content" style="margin-bottom: 0; text-align: left;text-indent: 2em; font-size: 10px;">{{
+                  data.sign
+                }}</p>
+            </a-comment>
+          </vue-scroll>
         </a-card>
       </a-card>
+    </a-card>
+    <a-card id="cnki" size="small" :head-style="{'text-align':'right'}"
+            :body-style="{'padding':'20px','padding-bottom':'0'}">
+      <div slot="title"><a-icon type="book"/>枝网查重</div>
+      happy
     </a-card>
   </div>
 </template>
@@ -35,6 +47,7 @@
 <script>
 import {parseTime} from "../../../utils/time";
 import {notification} from "ant-design-vue";
+import vueScroll from 'vuescroll';
 
 function copy(text) {
   const fakeElem = document.body.appendChild(
@@ -56,14 +69,93 @@ function copy(text) {
 
 export default {
   name: "commonTools",
+  components: {
+    vueScroll,
+  },
   data() {
     return {
       cfLoading: false,
       cfInputMessage: "",
       cfQueryList: [],
-      cfLoadedOnce:false,
+      cfQueryMessageJSONObject: {
+        "code": 0,
+        "message": 0,
+        "ttl": 0,
+        "data": {
+          "list": [
+            {
+              "mid": 0,
+              "attribute": 0,
+              "mtime": 0,
+              "tag": null,
+              "special": 0,
+              "contract_info": {
+                "is_contractor": false,
+                "ts": 0,
+                "is_contract": false
+              },
+              "uname": "",
+              "face": "",
+              "sign": "",
+              "official_verify": {
+                "type": 0,
+                "desc": ""
+              },
+              "vip": {
+                "vipType": 0,
+                "vipDueDate": 0,
+                "dueRemark": "",
+                "accessStatus": 0,
+                "vipStatus": 1,
+                "vipStatusWarn": "",
+                "themeType": 0,
+                "label": {
+                  "path": "",
+                  "text": "",
+                  "label_theme": "annual_vip",
+                  "text_color": "#FFFFFF",
+                  "bg_style": 1,
+                  "bg_color": "#FB7299",
+                  "border_color": ""
+                },
+                "avatar_subscript": 1,
+                "nickname_color": "#FB7299",
+                "avatar_subscript_url": " "
+              }
+            }]
+        }
+      },
+      cfLoadedOnce: false,
       zdLoading: false,
       zdInputMessage: "",
+      ops: {
+        vuescroll: {
+          mode: 'native',
+          sizeStrategy: 'percent',
+          detectResize: true,
+          locking: true,
+        },
+        scrollPanel: {
+          scrollingX: false,
+          scrollingY: true,
+          speed: 100,
+          easing: "easeInOutQuint",
+          maxHeight: "200"
+        },
+        rail: {
+          background: 'grey',
+          opacity: 0.2,
+          size: '6px',
+          gutterOfEnds: "10px",
+          gutterOfSide: '0px',
+        },
+        bar: {
+          keepShow: false,
+          background: 'black',
+          minSize: 0,
+          size: '6px',
+        }
+      }
     }
   },
   methods: {
@@ -95,7 +187,7 @@ export default {
     },
     cfSetSearchResponse(response) {
       const {list} = response;
-
+      this.$data.cfQueryMessageJSONObject = response;
       this.$data.cfQueryList = list || [];
     },
     cfQueryError(error) {
@@ -118,12 +210,17 @@ export default {
       });
       this.$data.cfLoading = false;
       this.$data.cfLoadedOnce = true;
+      console.log(this.$data.cfQueryMessageJSONObject);
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.ant-comment-inner {
+  padding: 0 !important;
+}
+
 #commonTools {
   display: flex;
   align-items: center;
@@ -131,6 +228,12 @@ export default {
   width: 100vw;
   height: 600px;
   background-color: grey;
+
+  /deep/ .ant-comment-inner {
+    padding-top: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
 
   #zhiJiangDictionary {
     width: 300px;
@@ -143,8 +246,15 @@ export default {
     #cfInnerCard {
       margin-top: 10px;
 
-      #cfInnerCardHeaderDivider {
-        margin-left: 0;
+      #cfQueryCellsWrapper {
+
+        #cfQueryCell {
+          text-align: left;
+
+          #cfQueryCellAvatar {
+
+          }
+        }
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import '../utils/request.js';
 import Meta from "vue-meta";
+
 Vue.config.productionTip = false
 Vue.use(Antd);
 Vue.use(less);
 Vue.use(Meta);
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
